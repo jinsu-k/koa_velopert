38781527483950fcc28b7f7642fa210ad39e09a0
Jinsu Kim <wlstn8738@naver.com>
2021-05-18 22:38:00 +0900

    Chore: Modularity Router

--- src/index.js
+++ src/index.js
@@ -3,28 +3,9 @@ const Router = require("koa-router");
 
 const app = new Koa();
 const router = new Router();
+const api = require("./api");
 
-router.get("/", (ctx, next) => {
-  ctx.body = "홈";
-});
-
-router.get("/about", (ctx, next) => {
-  ctx.body = "소개";
-});
-
-router.get("/about/:name", (ctx, next) => {
-  const { name } = ctx.params;
-  ctx.body = name + "의 소개";
-});
-
-router.get("/post", (ctx, next) => {
-  const { id } = ctx.request.query;
-  if (id) {
-    ctx.body = "포스트 #" + id;
-  } else {
-    ctx.body = "포스트 아이디가 없습니다.";
-  }
-});
+router.use("/api", api.routes());
 
 app.use(router.routes()).use(router.allowedMethods());
 
